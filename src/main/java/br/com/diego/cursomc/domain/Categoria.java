ebff57e1a1ab97681aff3a42da2e72ec049152df
diegoerrnane <diegocoutinho.e@gmail.com>
2022-02-27 23:42:40 -0300

    fix configurações do h2

--- src/main/java/br/com/diego/cursomc/domain/Categoria.java
+++ src/main/java/br/com/diego/cursomc/domain/Categoria.java
@@ -1,14 +1,25 @@
 package br.com.diego.cursomc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
+    public Categoria() {
+    }
+
     public Categoria(Integer id, String nome) {
+        super();
         this.id = id;
         this.nome = nome;
     }
